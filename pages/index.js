57fcfd1ecaacf16faf81a542033fc917a9ba0a8c
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2022-06-20 22:17:10 +0700

    Successfully call get all product by collection

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import ProductListings from "@/components/ProductListings";
 import StoreHeading from '@/components/StoreHeading'
-import { products } from "data";
+import { getAllProductsInCollection } from "@/lib/shopify";
+// import { products } from "data";
 
 
-export default function Home() {
+export default function Home({products}) {
+  console.log(products)
   return (
     <div>
       <StoreHeading />
       <ProductListings products={products} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const products = await getAllProductsInCollection()
+
+  return {
+    props: {
+      products
+    },
+  }
 }
